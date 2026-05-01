6297826931a535dee610a0bb04a1e3552b0c1b07
felipevarejao <felipe.tiago@icloud.com>
2026-05-01 04:19:00 +0000

    Update hello-world to v2:Hello from ArgoCD

--- hello-world/app.py
+++ hello-world/app.py
@@ -4,7 +4,7 @@ class HelloWorldHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b'Hello, World!')
+        self.wfile.write(b'Hello from ArgoCD :)')
 
 server = HTTPServer(('0.0.0.0', 8080), HelloWorldHandler)
 print('Server running on port 8080...')
